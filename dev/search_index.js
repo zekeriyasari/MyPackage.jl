333a7c7ac22a92b40bdeec8ef00daaeeecbf9f95
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-28 20:18:47 +0000

    build based on fe97850

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyPackage.jl-1","page":"Home","title":"MyPackage.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [MyPackage]","category":"page"},{"location":"#MyPackage.addone-Tuple{Any}","page":"Home","title":"MyPackage.addone","text":"addone(x)\n\nAdds one to x.\n\n\n\n\n\n","category":"method"},{"location":"#MyPackage.greet-Tuple{}","page":"Home","title":"MyPackage.greet","text":"greet()\n\nPrints a greetin message. \n\n\n\n\n\n","category":"method"}]
+}
